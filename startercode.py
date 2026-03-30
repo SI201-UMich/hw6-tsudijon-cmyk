a2c0be71951c7ff505df9d1d82378d2604eef3ed
Tiffany Sudijono <tiffanysudijono@227-3.synergydsl.com>
2026-03-29 21:06:39 -0400

    Finished load_json Function

--- startercode.py
+++ startercode.py
@@ -1,8 +1,8 @@
 # SI 201 HW6 (APIs, JSON, and Caching)
-# Your name:
-# Your student id:
-# Your email:
-# Who or what you worked with on this homework (including generative AI like ChatGPT):
+# Your name: Tiffany Phoebe Sudijono
+# Your student id: 53069112
+# Your email: tsudijon@umich.edu
+# Who or what you worked with on this homework (including generative AI like ChatGPT): I worked on this project independently, and did not use any form of Gen AI to complete the assignment.
 # If you worked with generative AI also add a statement for how you used it.
 # e.g.:
 # Asked ChatGPT for help debugging and understanding the JSON structure
@@ -36,7 +36,11 @@ def load_json(filename):
         A dictionary with the JSON data, OR an empty dictionary {} if the file
         cannot be opened or is not valid JSON.
     """
-    pass
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
 
 
 def create_cache(dictionary, filename):
